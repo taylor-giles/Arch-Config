f9c22662ee82ad7aaed397c913339b0b77d330d8
taylor-giles <taylor.g1823@gmail.com>
2021-11-27 13:16:45 -0500

    Fix select error

--- arch_install.sh
+++ arch_install.sh
@@ -1,36 +1,32 @@
 # Print out available disk options and save them to array
-lsblk -dplnx size -o name,size
-DISK_OPTIONS=("${(@f)$(lsblk -dplnx size -o name)}")
+DISK_OPTIONS=("${(@f)$(lsblk -dplnx size -o name,size)}")
 
 # Select disks
 PS3="Select the EFI disk: "
-select EFI_DISK from $DISK_OPTIONS
-do
-echo $EFI_DISK
-if [-z $EFI_DISK] then
-	echo "Please make a valid selection."
-else
-	break
+select EFI_DISK in $DISK_OPTIONS do
+	echo $EFI_DISK
+	if [-z $EFI_DISK] then
+		echo "Please make a valid selection."
+	else
+		break
 done
 
 PS3="Select the swap disk: "
-select SWAP_DISK from $DISK_OPTIONS
-do
-echo $SWAP_DISK
-if [-z $SWAP_DISK] then
-	echo "Please make a valid selection."
-else
-	break
+select SWAP_DISK in $DISK_OPTIONS do
+	echo $SWAP_DISK
+	if [-z $SWAP_DISK] then
+		echo "Please make a valid selection."
+	else
+		break
 done
 
 PS3="Select the filesystem disk: "
-select FS_DISK from $DISK_OPTIONS
-do
-echo $FS_DISK
-if [-z $FS_DISK] then
-	echo "Please make a valid selection."
-else
-	break
+select FS_DISK in $DISK_OPTIONS do
+	echo $FS_DISK
+	if [-z $FS_DISK] then
+		echo "Please make a valid selection."
+	else
+		break
 done
 
 
