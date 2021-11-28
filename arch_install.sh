a869fbaab8f56c2ca12d6b795ea9c1668f39170f
taylor-giles <taylor.g1823@gmail.com>
2021-11-27 20:00:28 -0500

    Partitions Selection

--- arch_install.sh
+++ arch_install.sh
@@ -1,22 +1,71 @@
 #!/usr/bin/env zsh
-# Print out available disk options and save them to array
-echo "Available disks:"
-lsblk -dplnx size -o name,size
-IFS=$'\n\n' DISK_OPTIONS=($(lsblk -dplnx size -o name))
 
-# Select disk
-PS3="Please select a disk to partition: "
-select CHOSEN_DISK in $DISK_OPTIONS 
+# Welcome
+echo "*************************************"
+echo "**** Welcome to the Taylor Giles ****"
+echo "******** Arch Linux Installer *******"
+echo "*************************************"
+echo "\n\n(Press Ctrl+C at any time to exit the installer.)"
+echo "\nIMPORTANT: This installer assumes that you already have EFI, SWAP, and filesystem partitions."
+echo "If you do not have these partitions prepared, please exit with Ctrl+C and partition now."
+
+# STEP 1: Set up Partitions
+while
 do
-	if [ -z $CHOSEN_DISK ]
-	then
-		echo "Please make a valid selection."
-	else
-		echo "Disk selected: $CHOSEN_DISK"
-		break
-	fi
+	echo "\n\n----- Step 1: Set Up Partitions -----"
+
+	# Print out available partition options and save them to array
+	echo "\nAvailable partitions:"
+	lsblk -dplnx size -o name,size
+	PARTITION_OPTIONS=(${(f)"${$(lsblk -l -n -o name,type | grep part)//part/" "}"})
+
+	# Select EFI partition
+	echo "\n"
+	PS3="Select the EFI partition: "
+	select EFI_PART in $PARTITION_OPTIONS 
+	do
+		if [ -z $EFI_PART ]
+		then
+			echo "Please make a valid selection."
+		else
+			echo "Partition selected: $EFI_PART"
+			break
+		fi
+	done
+
+	# Select SWAP partition
+	echo "\n"
+	PS3="Select the SWAP partition: "
+	select SWAP_PART in $PARTITION_OPTIONS 
+	do
+		if [ -z $SWAP_PART ]
+		then
+			echo "Please make a valid selection."
+		else
+			echo "Partition selected: $SWAP_PART"
+			break
+		fi
+	done
+
+	# Select FS partition
+	echo "\n"
+	PS3="Select the FILESYSTEM partition: "
+	select FS_PART in $PARTITION_OPTIONS 
+	do
+		if [ -z $FS_PART ]
+		then
+			echo "Please make a valid selection."
+		else
+			echo "Partition selected: $FS_PART"
+			break
+		fi
+	done
+
+	# Confirm choices
+	echo "Chosen partitions:\nEFI: $EFI_PART\nSWAP: $SWAP_PART\nFILESYSTEM: $FS_PART"
+	read -q "CONFIRM?Is this correct? ([y] to proceed)" && break
+	echo "\n\n"
 done
-echo ""
 
 
 
